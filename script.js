41ca2a5cf981587f45aa6f40637759a9a766342e
ni3tRinoo <vladik07@yahoo.com>
2023-07-14 17:29:52 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,55 @@
+// //1
+
+// let celsius = 10;
+// let fahrenheit = celsius * 1.8 + 32;
+
+// console.log(fahrenheit);
+
+// //2
+
+// let DaysInMonth = 30;
+// let HoursInMonth = DaysInMonth * 24;
+// let MinutesInMonth = HoursInMonth *60;
+
+// console.log(HoursInMonth + "hours");
+// console.log(HoursInMonth + "minutes");
+
+// //3
+
+// let health = 100;
+// let power = 100;
+
+// console.log(health-10);
+// console.log(power-10);
+
+// //4
+
+// let total = 1500;
+// let discount = total *10 / 100;
+// console.log("To pay:", + total-discount);
+
+// //5
+
+// let number = 15.792;
+// console.log(Math.floor(number));
+
+// //6
+
+// let number = 104.23;
+// console.log(parseFloat(number))
+
+//7
+
+// let string = 64.498;
+// console.log(parseInt(string))
+
+// //8
+
+// let number = 64;
+// console.log(Math.sqrt(number));
+
+// //9
+
+// let string = 100.104;
+// console.log(parseInt(string))
+// console.log(toString(string))
